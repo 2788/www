c7368d5b8cf04054bc3cf1ec1103561bee0442f4
Miclle Zheng <miclle.zheng@gmail.com>
2017-04-17 12:00:01 +0800

    Update application config.

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,20 @@ module Official
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.mongoid.logger = Logger.new($stdout, :warn)
+
+    config.action_dispatch.perform_deep_munge = false
+
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Beijing'
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = "zh-CN"
+
+    config.exceptions_app = self.routes
   end
 end
