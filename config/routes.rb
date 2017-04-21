ba7994ee5d8990669bfa8cba2911d3fddcf4cc8a
Miclle Zheng <miclle.zheng@gmail.com>
2017-04-21 19:14:17 +0800

    Support multi domains.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
+require 'domain_constraint'
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'welcome#index'
-
-  constraints subdomain: 'career-source' do
+  constraints DomainConstraint.new('career', 'career-source') do
     root to: 'career#index'
   end
 
-  # constraints subdomain: 'blog' do
-  #   resources :articles
-  # end
+  constraints DomainConstraint.new('blog', 'blog-source') do
+    root to: 'articles#index'
+  end
+
+  root to: 'welcome#index'
 
 end
