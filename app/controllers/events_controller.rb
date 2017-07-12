9f27d88200976a179334012bf6090078120d8da3
jinfeng <jinfenggt@gmail.com>
2017-07-12 11:17:16 +0800

    update market event display

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,19 +1,22 @@
 class EventsController < ApplicationController
   def index
+    @niushows = Event.where( category: 'NiuShow').limit(3)
+    @teches = Event.where( category: 'TechOnline').limit(3)
+    @ecugs = Event.where( category: 'ECUG').limit(3)
   end
 
   def tech_online
-    @events = Event.where( category: 'tech_online')
+    @events = Event.where( category: 'TechOnline').limit(9)
     render "list"
   end
 
   def ecug
-    @events = Event.where( category: 'ecug')
+    @events = Event.where( category: 'ECUG').limit(9)
     render "list"
   end
 
   def niushow
-    @events = Event.where( category: 'niushow')
+    @events = Event.where( category: 'NiuShow').limit(9)
     render "list"
   end
 end
