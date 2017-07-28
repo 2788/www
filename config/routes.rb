aa522d5ce5ffe74e10e7f20ef86a93e5cc814bb1
jinfeng <jinfenggt@gmail.com>
2017-07-28 14:22:09 +0800

    add 404 page

--- config/routes.rb
+++ config/routes.rb
@@ -54,4 +54,10 @@ Rails.application.routes.draw do
     end
   end
 
+
+  get "robots.txt" => "welcome#robots"
+
+  match "/404", :to => "errors#not_found", :via => :all
+  match "/500", :to => "errors#internal_server_error", :via => :all
+
 end
