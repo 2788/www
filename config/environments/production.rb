9b2ddbaa8fe0e96ea5358c148ae63a06f91b2d1f
momaek <momaek17@gmail.com>
2017-08-17 10:33:50 +0800

    backend finish

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,9 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
+  config.acc_host = 'https://acc.qbox.me'
+  config.sso_host = 'https://sso.qiniu.com'
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
