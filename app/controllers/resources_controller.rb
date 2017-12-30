259363fd0db108a57f7b4ebb5d9e60ae88ce1892
Miclle Zheng <miclle.zheng@gmail.com>
2017-12-30 12:26:39 +0800

    Update plup page style

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -2,7 +2,7 @@ class ResourcesController < ApplicationController
 
   # https://www.qiniu.com/resources/plup
   def plup
-    @top_archives = Archive.top_archives
+    @title = "七牛云“涉稳安全资源包”火速到场"
   end
 
 end
