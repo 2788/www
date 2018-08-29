0201a2b73145686f445c0a5f1d58ed6dfc07b5f2
war408705279 <408705279@qq.com>
2018-08-29 15:53:35 +0800

    Update welcom_helper && Modify about page

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,8 +1,12 @@
 module WelcomeHelper
 
   # 校验welcome/index页面 top_archives 发布信息字段
-  def archive_publish_meta(archive)
+  def index_archive_publish_meta(archive)
     "#{ archive[:created_at].strftime("%Y-%m-%d") } 发布在 #{ Archive::CATEGORIES[archive.category] || "" }"
   end
 
+  # 校验welcome/about页面 archives 发布信息字段
+  def about_archive_publish_meta(archive)
+    "发布在 #{ Archive::CATEGORIES[archive.category] || "" }"
+  end
 end
