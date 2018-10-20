e9fe014b83861bbbe6f2ef09108aeef9c3e1cb2e
war408705279 <408705279@qq.com>
2018-10-20 16:47:53 +0800

    BO-5294 1024 活动页面文案/图片更新 && 接口联调

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,6 +53,8 @@ Rails.application.configure do
   config.sso_host = 'https://sso-dev.qiniu.io'
   config.www_host = 'http://www-dev.qiniu.io'
   config.blog_host = 'http://blog-dev.qiniu.io'
+  config.portal_host = 'http://portalv4.dev.qiniu.io'
+  config.gaea_admin_host = 'http://bo-gaea-admin-spock-public.cs-spock.cloudappl.com'
 
   config.img_audit_host = 'https://argus.atlab.ai/v1/image/censor'
   config.video_audit_host = 'https://argus.atlab.ai/v1/video/'
