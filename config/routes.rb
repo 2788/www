002e8cd795e0738bda35b6a4088faef8fdb247a0
war408705279 <408705279@qq.com>
2018-11-04 09:49:29 +0800

    BO-5441 官网代码组织结构梳理（dora 页面）

--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,7 @@ Rails.application.routes.draw do
     get '/products/insight',        to: 'products#insight'
     get '/products/censor',         to: 'welcome#censor'
     get '/products/fusion',         to: 'products#fusion'
-    get '/products/dora',           to: 'welcome#dora'
+    get '/products/dora',           to: 'products#dora'
     get '/products/kirk',           to: 'products#kirk'
     get '/products/vance',          to: 'welcome#vance'
     get '/products/atlab',          to: 'products#atlab'
