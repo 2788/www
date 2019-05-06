4e116d550d961abbd624d7a8f7cb084e5d3af2bd
war408705279 <408705279@qq.com>
2019-05-06 13:54:24 +0800

    BO-7102 添加云短信页面

--- config/routes.rb
+++ config/routes.rb
@@ -62,6 +62,7 @@ Rails.application.routes.draw do
     get '/products/private-cloud-kodo',            to: 'products#kodoprivate'
     get '/products/private-cloud-kodo/company',    to: 'products#kodoprivate_company'
     get '/products/linking',                       to: 'products#linking'
+    get '/products/sms',                           to: 'products#sms'
 
     # 推广结束，屏蔽 qvmfeeds 和 censorfeeds 的路由
     # https://jira.qiniu.io/browse/BO-5919
