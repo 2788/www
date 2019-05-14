9d5df6be8b12c841e40245ae18fce489ee60fe0b
war408705279 <408705279@qq.com>
2019-05-14 15:13:51 +0800

    BO-7137 更新白皮书下载页面下载逻辑

--- app/controllers/download_controller.rb
+++ app/controllers/download_controller.rb
@@ -4,41 +4,49 @@ class DownloadController < ApplicationController
 
   def snow_white_paper
     @product = "snow"
+    @path = request.original_fullpath
     render "index"
   end
 
   def plsv_white_paper
     @product = "plsv"
+    @path = request.original_fullpath
     render "index"
   end
 
   def kodo_white_paper
     @product = "kodo"
+    @path = request.original_fullpath
     render "index"
   end
 
   def atlab_white_paper
     @product = "atlab"
+    @path = request.original_fullpath
     render "index"
   end
 
   def rtn_white_paper
     @product = "rtn"
+    @path = request.original_fullpath
     render "index"
   end
 
   def kodoe_white_paper
     @product = "kodoe"
+    @path = request.original_fullpath
     render "index"
   end
 
   def pili_white_paper
     @product = "pili"
+    @path = request.original_fullpath
     render "index"
   end
 
   def qavs_white_paper
     @product = "qavs"
+    @path = request.original_fullpath
     render "index"
   end
 
