411e898ecc4ba0b4c895aa46da0b59c317e5e339
war408705279 <408705279@qq.com>
2019-09-29 14:32:06 +0800

    BO-9032 官网企业上云活动下线

--- config/routes.rb
+++ config/routes.rb
@@ -121,7 +121,7 @@ Rails.application.routes.draw do
     
     resources 'events', only: [:index] do
       collection do
-        get 'tech_online', 'ecug', 'niushow', 'arch', 'ecugcon', 'free', 'qvm1rmb', 'qvm0rmb', 'double11', 'qvm2for3', 'enterpriseoncloud'
+        get 'tech_online', 'ecug', 'niushow', 'arch', 'ecugcon', 'free', 'qvm1rmb', 'qvm0rmb', 'double11', 'qvm2for3'
       end
     end
 
