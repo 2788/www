50c5d4cbcd303c751dacb62c8fa35525c3ec11f4
war408705279 <408705279@qq.com>
2020-03-27 14:12:11 +0800

    rename && fix code by review

--- front/marketing/build-version.sh
+++ front/marketing/build-version.sh
@@ -3,14 +3,14 @@ basename=$1
 host="//qiniu.staticfile.org"
 polyfill=$(ls $basename/fec__polyfill-*.js)
 polyfillName=${polyfill##*/}
-script=$(ls $basename/www-marketing-*.js)
-scriptName=${script##*/}
+renderer=$(ls $basename/www-marketing-*.js)
+rendererName=${renderer##*/}
 css=$(ls $basename/www-marketing-*.css)
 cssName=${css##*/}
 cat > $basename/version.js << END_TEXT
 window['__qiniuWWWMarketingVersion__'] = {
   polyfillUrl: '${host}/static/${polyfillName}',
-  scriptUrl: '${host}/static/${scriptName}',
+  rendererUrl: '${host}/static/${rendererName}',
   cssUrl: '${host}/static/${cssName}'
 }
 END_TEXT
