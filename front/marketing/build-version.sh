2111179ac4a03001ba01320ac0ae935a39013248
war408705279 <408705279@qq.com>
2020-03-27 11:34:03 +0800

    添加 polyfill js 版本控制

--- front/marketing/build-version.sh
+++ front/marketing/build-version.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 basename=$1
 host="//qiniu.staticfile.org"
+polyfill=$(ls $basename/fec__polyfill-*.js)
+polyfillName=${polyfill##*/}
 script=$(ls $basename/www-marketing-*.js)
 scriptName=${script##*/}
 css=$(ls $basename/www-marketing-*.css)
 cssName=${css##*/}
 cat > $basename/version.js << END_TEXT
 window['__qiniuWWWMarketingVersion__'] = {
+  polyfillUrl: '${host}/static/${polyfillName}',
   scriptUrl: '${host}/static/${scriptName}',
   cssUrl: '${host}/static/${cssName}'
 }
