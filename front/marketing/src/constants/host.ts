d8811e941aea78b5eed84624462747c181359285
war408705279 <408705279@qq.com>
2020-04-03 11:07:46 +0800

    去除领券成功提示模态框改为 toaster 提醒 && 样式修改

--- front/marketing/src/constants/host.ts
+++ front/marketing/src/constants/host.ts
@@ -7,14 +7,12 @@ import { isDev } from './env'
 
 enum portalHosts {
   Development = 'http://portalv4.dev.qiniu.io', // 测试环境
-  // Production = 'https://portal.qiniu.com' // 线上环境
-  Production = 'http://portalv4.dev.qiniu.io' // TODO 这边为了方便 spock 测试改成了测试环境的地址
+  Production = 'https://portal.qiniu.com' // 线上环境
 }
 
 enum ssoHosts {
   Development = 'https://sso-dev.qiniu.io', // 测试环境
-  // Production = 'https://sso.qiniu.com' // 线上环境
-  Production = 'https://sso-dev.qiniu.io' // TODO 这边为了方便 spock 测试改成了测试环境的地址
+  Production = 'https://sso.qiniu.com' // 线上环境
 }
 
 export const portalHost = isDev ? portalHosts.Development : portalHosts.Production
