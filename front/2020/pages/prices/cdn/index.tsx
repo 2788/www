95b9c7b0b03e1465af3e2183627b8ab3bb236c36
黄斌杰 <akira.binjie@gmail.com>
2020-05-15 16:24:34 +0800

    大小写

--- front/2020/pages/prices/cdn/index.tsx
+++ front/2020/pages/prices/cdn/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Layout from 'components/Price/Layout'
 import Banner from 'components/Price/Banner'
-import PricePane from 'components/Price/cdn/price'
-import CalcPane from 'components/Price/Banner/CalcPane'
+import PricePane from 'components/Price/cdn/Price'
+import CalcPane from 'components/Price/cdn/Calc'
 
 function Page() {
   return (
