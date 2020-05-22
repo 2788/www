fd78a54d2088862fcf2533ec5542a63e603d780c
黄斌杰 <akira.binjie@gmail.com>
2020-05-22 18:06:44 +0800

    kodo 计算器

--- front/2020/pages/prices/kodo/index.tsx
+++ front/2020/pages/prices/kodo/index.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Layout from 'components/Price/Layout'
 import Banner from 'components/Price/Banner'
 import PricePane from 'components/Price/kodo/Price'
+import CalcPane from 'components/Price/kodo/Calc'
 
 function Page() {
   return (
     <>
       <Banner product="价格 | 对象存储">
         <PricePane />
+        <CalcPane />
       </Banner>
     </>
   )
