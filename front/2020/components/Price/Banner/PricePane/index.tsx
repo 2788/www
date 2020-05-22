b83d0de24a9c98401b8edeeab0cf00d478b6f8e9
黄斌杰 <akira.binjie@gmail.com>
2020-05-22 18:36:32 +0800

    注册机制 & 以图搜图

--- front/2020/components/Price/Banner/PricePane/index.tsx
+++ front/2020/components/Price/Banner/PricePane/index.tsx
@@ -1,9 +1,14 @@
-import React, { PropsWithChildren } from 'react'
+import React, { PropsWithChildren, useEffect, useContext } from 'react'
 import { Pane } from '../Pane'
+import { BannerContext } from '..'
 
 export * from './PricePaneSection'
 
 export default function PricePane({ children }: PropsWithChildren<{}>) {
+  const { registerPane } = useContext(BannerContext)
+
+  useEffect(() => registerPane('price'), [registerPane])
+
   return (
     <Pane name="price">{children}</Pane>
   )
