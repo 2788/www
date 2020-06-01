f840090980f318792a87fde9843ca256e80baeb7
geograous <geograous@126.com>
2020-06-01 11:05:20 +0800

    修改cdn页面 (#1691)

--- front/2020/components/pages/qcdn/Packages/index.tsx
+++ front/2020/components/pages/qcdn/Packages/index.tsx
@@ -20,7 +20,7 @@ export default function Packages() {
         capacity="500 GB"
         categoryDesc="每月发放 20 GB，连续发放 5 个月"
         price="7.7"
-        detailLink="/prices?entry=kodo-page"
+        detailLink="/prices/qcdn?entry=kodo-page"
         sellPoints={['适用时段：全时段可用', '类型：HTTP']}
       />
       <Card
@@ -30,7 +30,7 @@ export default function Packages() {
         capacity="500 GB"
         categoryDesc="一次性发放，一年有效"
         price="98"
-        detailLink="/prices?entry=kodo-page"
+        detailLink="/prices/qcdn?entry=kodo-page"
         sellPoints={['适用时段：全时段可用', '类型：HTTP']}
       />
       <Card
@@ -40,7 +40,7 @@ export default function Packages() {
         capacity="1 TB"
         categoryDesc="一次性发放，一年有效"
         price="193"
-        detailLink="/prices?entry=kodo-page"
+        detailLink="/prices/qcdn?entry=kodo-page"
         sellPoints={['适用时段：全时段可用', '类型：HTTP']}
       />
       <Card
@@ -50,7 +50,7 @@ export default function Packages() {
         capacity="5 TB"
         categoryDesc="一次性发放，一年有效"
         price="942"
-        detailLink="/prices?entry=kodo-page"
+        detailLink="/prices/qcdn?entry=kodo-page"
         sellPoints={['适用时段：全时段可用', '类型：HTTP']}
       />
     </Section>
@@ -103,7 +103,7 @@ function Card(props: CardProps) {
           <span className={style.number}>{price}</span>
           <span>{unit}</span>
         </p>
-        <Button className={style.button} href={detailLink} withBorder>
+        <Button className={style.button} href={detailLink} type="hollow" withBorder>
           了解详情
         </Button>
       </div>
