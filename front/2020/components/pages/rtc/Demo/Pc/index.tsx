4e9bb357715b9d255ec7ca6bc0da0af977b85349
Hanxing Yang <nighca@live.cn>
2020-06-05 11:58:54 +0800

    RTN 页面同步线上二维码 & 改为 JS 生成 (#1734)

--- front/2020/components/pages/rtc/Demo/Pc/index.tsx
+++ front/2020/components/pages/rtc/Demo/Pc/index.tsx
@@ -1,25 +1,33 @@
 import React from 'react'
+import QRCode from 'qrcode.react'
 
-// TODO js 生成 qrcode
-import ios from './images/iOS.png'
-import android from './images/Android.png'
 import style from './index.less'
 
-// TODO 移动端
 export default function Pc() {
   return (
     <>
       <figure className={style.figure}>
-        <img src={ios} />
+        <MyQRCode value="http://fir.qnsdk.com/517z?utm_source=fir&utm_medium=qr" />
         <figcaption>扫码体验 iOS Demo</figcaption>
       </figure>
       <figure className={style.figure}>
-        <img src={android} />
+        <MyQRCode value="http://fir.qnsdk.com/b5ce" />
         <figcaption>扫码体验 Android Demo</figcaption>
       </figure>
       <div className={style.webDemo}>
         <a className={style.link} href="https://demo-rtc.qnsdk.com/">Web 端在线体验</a>
       </div>
     </>
+  )
+}
+
+function MyQRCode(props: { value: string }) {
+  return (
+    <QRCode
+      className={style.qrCode}
+      renderAs="svg"
+      fgColor="#333"
+      {...props}
+    />
   )
 }
