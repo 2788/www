740c3ca3e75e537e64a476d605e9891c273d9e4c
geograous <geograous@126.com>
2020-06-08 10:43:49 +0800

    修改menu打开，修改样式 (#1741)
    * 更新 menu 折叠，修改样式
    
    (cherry picked from commit d571af248c67814c73a88e9c7bfa057da8dd7aba)
    
    * 更换节点覆盖世界地图图片
    
    (cherry picked from commit 01adcbf0d8e4c65c4f5ae186923b49234473e732)
    
    * 调整css样式
    
    * change menu impl
    
    * update

--- front/2020/components/UI/Menu/index.tsx
+++ front/2020/components/UI/Menu/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classnames from 'classnames'
 import RcMenu from 'rc-menu'
 import { MenuProps as RcMenuProps } from 'rc-menu/es/Menu'
@@ -14,12 +14,44 @@ interface MenuProps extends RcMenuProps {
   inlineIndent?: number
 }
 
-export default function Menu({ inlineIndent = 16, ...rest }: MenuProps) {
+export default function Menu({ inlineIndent = 16, openKeys, ...rest }: MenuProps) {
+  const [localOpenKeys, setLocalOpenKeys] = useState<string[]>([])
+
   function expandIcon(props: any) {
     if (props.isOpen) {
       return <ArrowDownIcon className={classnames(style.icon, style.open)} />
     }
     return <ArrowDownIcon className={style.icon} />
   }
-  return <RcMenu {...{ inlineIndent }} expandIcon={expandIcon} {...rest} />
+
+  // 外部控制 openKeys
+  if (openKeys != null) {
+    return (
+      <RcMenu
+        {...{ inlineIndent }}
+        expandIcon={expandIcon}
+        openKeys={openKeys}
+        {...rest}
+      />
+    )
+  }
+
+  function handleOpenChange(currentOpenKeys: any) {
+    const newLocalOpenKeys: string[] = []
+    if (currentOpenKeys.length > 0) {
+      newLocalOpenKeys.push(currentOpenKeys.pop())
+      setLocalOpenKeys(newLocalOpenKeys)
+    }
+  }
+
+  return (
+    <RcMenu
+      {...{ inlineIndent }}
+      expandIcon={expandIcon}
+      onOpenChange={handleOpenChange}
+      openKeys={localOpenKeys}
+      {...rest}
+    />
+  )
+
 }
