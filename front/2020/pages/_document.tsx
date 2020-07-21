a5c23d4c2b1935a7774021bec9c14f782c69a6ff
黄斌杰 <501711499@qq.com>
2020-07-21 17:25:36 +0800

    fouc (#1915)
    * init
    
    * 调整顺序，读取不到 body
    
    * 测试 script to block 的方式
    
    * 最终版本

--- front/2020/pages/_document.tsx
+++ front/2020/pages/_document.tsx
@@ -1,4 +1,4 @@
-// eslint-disable react/no-danger
+/* eslint-disable react/no-danger */
 
 import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
@@ -82,6 +82,8 @@ class MyDocument extends Document {
           <script dangerouslySetInnerHTML={{ __html: baiduzhanzhangScriptContent }} />
         </Head>
         <body>
+          {/* https://stackoverflow.com/a/57888310 */}
+          <script> </script>
           <Main />
           <NextScript />
           {/* https://stackoverflow.com/a/42969608 修复 Chrome 下 transition 会立即触发的问题 */}
