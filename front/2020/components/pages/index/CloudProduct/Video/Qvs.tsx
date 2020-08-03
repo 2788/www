cf9749e02921b876a16963d5c2c611c173d07cc3
黄斌杰 <akira.binjie@gmail.com>
2020-08-03 11:01:55 +0800

    去掉 QVS 后面的线

--- front/2020/components/pages/index/CloudProduct/Video/Qvs.tsx
+++ front/2020/components/pages/index/CloudProduct/Video/Qvs.tsx
@@ -3,13 +3,11 @@ import Link from 'components/Link'
 import style from '../index.less'
 import { Node } from '..'
 import Positioned from '../Positioned'
-import Line from '../Line'
 import SDKIcon from '../icons/media/SDK'
 
 export default function Qvs() {
   return (
     <>
-      <Positioned top={458} left={146}><Line width={27} /></Positioned>
       <Positioned identity={Node.Sdk} top={405} left={184}>
         <svg width="144" height="165" viewBox="0 0 144 165">
           <SDKIcon />
