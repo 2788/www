76b2a85f7cc07423e9591bee9d166e0cbfa3975a
黄斌杰 <akira.binjie@gmail.com>
2020-08-11 17:51:21 +0800

    openapi 产品页 pc 端

---
+++ front/2020/components/pages/openapi/Doc/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import LinkGroups, { LinkGroup, LinkItem } from 'components/Product/LinkGroups'
+
+export default function Doc() {
+  return (
+    <LinkGroups title="产品文档">
+      <LinkGroup title="使用文档">
+        <LinkItem href="https://developer.qiniu.com/dora/api/3688/the-third-party-data-processing">产品简介</LinkItem>
+      </LinkGroup>
+      <LinkGroup title="产品推荐">
+        <LinkItem href="https://www.qiniu.com/products/dora">智能多媒体服务</LinkItem>
+        <LinkItem href="https://www.qiniu.com/products/censor">内容审核</LinkItem>
+        <LinkItem href="https://www.qiniu.com/products/faceid">人脸核验</LinkItem>
+      </LinkGroup>
+    </LinkGroups>
+  )
+}
