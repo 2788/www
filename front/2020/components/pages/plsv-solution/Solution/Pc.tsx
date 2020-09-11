91c6fe454c98cb0559234d0eef7f09fb9b739dd5
Hanxing Yang <nighca@live.cn>
2020-09-11 16:52:05 +0800

    移除播放器 SDK 相关内容 (#1999)

--- front/2020/components/pages/plsv-solution/Solution/Pc.tsx
+++ front/2020/components/pages/plsv-solution/Solution/Pc.tsx
@@ -9,7 +9,6 @@ import React from 'react'
 import SolutionIconOne from './solution-icon-one.svg'
 import SolutionIconTwo from './solution-icon-two.svg'
 import SolutionIconThree from './solution-icon-three.svg'
-import SolutionIconFour from './solution-icon-four.svg'
 import SolutionIconFive from './solution-icon-five.svg'
 
 import styles from './style.less'
@@ -69,27 +68,6 @@ export default function Pc() {
               </div>
             </div>
           </div>
-          <div className={styles.item}>
-            <SolutionIconFour className={styles.icon} />
-            <div className={styles.content}>
-              <div className={styles.title}>极致播放体验</div>
-              <div className={styles.desc}>
-                提供免费的播放器 SDK，秒开 200 ms，包体小至 400 K，千万日活验证，给用户流畅无卡顿的视频体验。
-              </div>
-              <div className={styles.desc}>
-                相关文档：
-                <a
-                  className={styles.link}
-                  href="/products/player"
-                  title="播放器 SDK"
-                >
-                  播放器 SDK
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className={styles.group}>
           <div className={styles.item}>
             <SolutionIconFive className={styles.icon} />
             <div className={styles.content}>
