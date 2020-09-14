696c02ddda59633454ea4893a9856b3af4c40257
黄斌杰 <akira.binjie@gmail.com>
2020-09-14 16:25:44 +0800

    小程序不显示 UsageGuide

--- front/2020/components/Product/UsageGuide/index.tsx
+++ front/2020/components/Product/UsageGuide/index.tsx
@@ -5,7 +5,6 @@
 
 import React, { ReactNode } from 'react'
 import { useMp } from 'hooks/ua'
-import { showMpModal } from 'components/mp/Toast'
 
 import UIButton, { Props as UIButtonProps } from '../../UI/Button'
 import style from './style.less'
@@ -17,6 +16,10 @@ export type Props = {
 }
 
 export default function UsageGuide({ title, description, children }: Props) {
+  if (useMp()) {
+    return null
+  }
+
   return (
     <div className={style.wrapper}>
       <div className={style.content}>
@@ -35,16 +38,10 @@ export default function UsageGuide({ title, description, children }: Props) {
 export type ButtonProps = UIButtonProps
 
 export function Button(props: ButtonProps) {
-  const isMp = useMp()
   const className = [
     props.className,
     style.button
   ].filter(Boolean).join(' ')
-
-  if (isMp) {
-    const { href, ...mpProps } = props
-    return <UIButton {...mpProps} onClick={() => showMpModal('为更好的用户体验，请登录电脑端管理控制台操作')} className={className} />
-  }
 
   return <UIButton {...props} className={className} />
 }
