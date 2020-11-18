79beff84395ed4680d3de4a3de304e699cb2abdd
Alex Yan <yanhao1991@gmail.com>
2020-11-18 18:53:58 +0800

    change qvm calc url (#2089)

--- front/2020/components/Price/qvm/Calc/index.tsx
+++ front/2020/components/Price/qvm/Calc/index.tsx
@@ -6,7 +6,7 @@ import style from './index.less'
 export default function CdnCalc() {
   return (
     <CalcPane>
-      <iframe className={style.iframe} src="https://calc.qvm-dev.online/?mode=user"></iframe>
+      <iframe className={style.iframe} src="https://calc-qvm.qiniu.com/?mode=user"></iframe>
     </CalcPane>
   )
 }
