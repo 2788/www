09cdc381ab291bb94f7c6374e14c4cdc69f38920
任攀攀 <34532573+renpanpan5@users.noreply.github.com>
2020-12-09 19:05:06 +0800

    cdn 删除国内地图 (#2105)
    Co-authored-by: renpanpan5 <renpanpan5@github.com>

--- front/2020/components/pages/qcdn/Coverage/index.tsx
+++ front/2020/components/pages/qcdn/Coverage/index.tsx
@@ -3,10 +3,9 @@
  * @author zhuhao <zhuhao@qiniu.com>
  */
 
-import React, { ReactNode, PropsWithChildren, useState } from 'react'
+import React, { ReactNode, PropsWithChildren } from 'react'
 import classnames from 'classnames'
 
-import ButtonRadioGroup, { ButtonRadio } from 'components/UI/ButtonRadio'
 import Section from 'components/Product/Section'
 
 import NodeIcon1 from './images/node1.svg'
@@ -14,24 +13,16 @@ import NodeIcon2 from './images/node2.svg'
 import NodeIcon3 from './images/node3.svg'
 import NodeIcon4 from './images/node4.svg'
 
-import ChinaMapImage from './images/map_china.png'
 import WorldMapImage from './images/map_world.png'
 
 import styles from './style.less'
 
 enum CoverageType {
-  China = 'china',
   World = 'world'
 }
 
 const coverageImageMap = {
-  [CoverageType.China]: ChinaMapImage,
   [CoverageType.World]: WorldMapImage
-}
-
-const coverageNameTextMap = {
-  [CoverageType.China]: '国内节点覆盖',
-  [CoverageType.World]: '国际节点覆盖'
 }
 
 interface PointProps {
@@ -48,7 +39,6 @@ function Point({ icon, children }: PropsWithChildren<PointProps>) {
 }
 
 export default function Coverage() {
-  const [coverageType, setCoverageType] = useState(CoverageType.China)
   return (
     <Section header="国内外优质节点覆盖，用户随处可用" title="节点覆盖" name="coverage">
       <div className={styles.coverage}>
@@ -58,26 +48,8 @@ export default function Coverage() {
           <Point icon={<NodeIcon3 />}>访问提速 <span className={styles.highlight}>80%</span></Point>
           <Point icon={<NodeIcon4 />}><span className={styles.highlight}>100 万</span> 客户选择</Point>
         </ul>
-        <div className={classnames(styles.map, coverageType === CoverageType.China ? styles.china : styles.world)}>
-          <img className={styles.mapImage} src={coverageImageMap[coverageType]} />
-          <ButtonRadioGroup
-            className={styles.radios}
-            value={coverageType}
-            onChange={t => setCoverageType(t as CoverageType)}
-          >
-            <ButtonRadio
-              className={styles.radio}
-              value={CoverageType.China}
-            >
-              {coverageNameTextMap[CoverageType.China]}
-            </ButtonRadio>
-            <ButtonRadio
-              className={styles.radio}
-              value={CoverageType.World}
-            >
-              {coverageNameTextMap[CoverageType.World]}
-            </ButtonRadio>
-          </ButtonRadioGroup>
+        <div className={classnames(styles.map, styles.world)}>
+          <img className={styles.mapImage} src={coverageImageMap[CoverageType.World]} />
         </div>
       </div>
     </Section>
