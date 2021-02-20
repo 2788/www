8f4194647a404ad50055312b8fd74e89b4ffe4a7
任攀攀 <34532573+renpanpan5@users.noreply.github.com>
2021-02-20 18:04:42 +0800

    去除小红书和脉脉 (#2189)
    Co-authored-by: renpanpan5 <renpanpan5@github.com>

--- front/2020/components/pages/qnplayer/Case/index.tsx
+++ front/2020/components/pages/qnplayer/Case/index.tsx
@@ -5,7 +5,7 @@
 
 import React from 'react'
 import CustomerCaseGroup, { CustomerCase } from 'components/Product/CustomerCaseGroup'
-import { blue, xhs, qczj, cy, mtime, maimai } from 'constants/cases'
+import { blue, qczj, cy, mtime } from 'constants/cases'
 
 export default function QnPlayerCase() {
   return (
@@ -13,9 +13,7 @@ export default function QnPlayerCase() {
       <CustomerCase pic={blue.logo} />
       <CustomerCase pic={mtime.logo} />
       <CustomerCase pic={cy.logo} />
-      <CustomerCase pic={maimai.logo} />
       <CustomerCase pic={qczj.logo} />
-      <CustomerCase pic={xhs.logo} />
     </CustomerCaseGroup>
   )
 }
