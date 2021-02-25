097b7b9a784a174532f6e2507ba859858ff0449c
war408705279 <408705279@qq.com>
2021-02-25 15:00:57 +0800

    Header 组件调用后端接口种下 cps_key cookie && 修改 TODO 内容

--- front/2020/components/Header/index.tsx
+++ front/2020/components/Header/index.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { useMobile } from 'hooks/ua'
 import { useQueryValue } from 'hooks/url'
+import { useApi } from 'hooks/api'
+
+import { setCpsKeyCookie } from 'apis/cps'
 
 import Pc from './Pc'
 import Mobile from './Mobile'
@@ -10,18 +13,20 @@ export default function Header() {
   const isMobile = useMobile()
 
   // 如果 url 有 cps_key query 参数
-  // 则种下对应的 cookie
+  // 则调用后端接口种下对应的 cookie
   // https://jira.qiniu.io/browse/BO-15948
-  // TODO: 先临时支持一下，后续再优化
+  // TODO: 后续将对应逻辑迁移到 externals 中
+  // https://jira.qiniu.io/browse/BO-16041
   const [cpsKey] = useQueryValue('cps_key', '')
-  if (cpsKey) {
-    const hostname = window.location.hostname
-    const domain = hostname.split('.').slice(-2).join('.')
-    const date = new Date()
-    // cps_key cookie 有效期为 7 天
-    date.setTime(date.getTime() + (7 * 24 * 60 * 60 * 1000))
-    document.cookie = `cps_key=${cpsKey}; domain=${domain}; path=/; expires=${date.toUTCString()}`
-  }
+  const { call: callSetCpsKeyCookie } = useApi(setCpsKeyCookie)
+
+  useEffect(() => {
+    if (!cpsKey) return
+
+    callSetCpsKeyCookie({
+      cps_key: cpsKey
+    })
+  }, [cpsKey, callSetCpsKeyCookie])
 
   return isMobile ? <Mobile /> : <Pc />
 }
