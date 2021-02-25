c1645b55e0dd44610729bc9f76b6b7c7756aa869
war408705279 <408705279@qq.com>
2021-02-25 15:13:20 +0800

    移动相关逻辑至 hooks/cps 中

--- front/2020/components/Header/index.tsx
+++ front/2020/components/Header/index.tsx
@@ -1,10 +1,7 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 
 import { useMobile } from 'hooks/ua'
-import { useQueryValue } from 'hooks/url'
-import { useApi } from 'hooks/api'
-
-import { setCpsKeyCookie } from 'apis/cps'
+import { useSetCpsKeyCookie } from 'hooks/cps'
 
 import Pc from './Pc'
 import Mobile from './Mobile'
@@ -12,21 +9,7 @@ import Mobile from './Mobile'
 export default function Header() {
   const isMobile = useMobile()
 
-  // 如果 url 有 cps_key query 参数
-  // 则调用后端接口种下对应的 cookie
-  // https://jira.qiniu.io/browse/BO-15948
-  // TODO: 后续将对应逻辑迁移到 externals 中
-  // https://jira.qiniu.io/browse/BO-16041
-  const [cpsKey] = useQueryValue('cps_key', '')
-  const { call: callSetCpsKeyCookie } = useApi(setCpsKeyCookie)
-
-  useEffect(() => {
-    if (!cpsKey) return
-
-    callSetCpsKeyCookie({
-      cps_key: cpsKey
-    })
-  }, [cpsKey, callSetCpsKeyCookie])
+  useSetCpsKeyCookie()
 
   return isMobile ? <Mobile /> : <Pc />
 }
---
+++ front/2020/hooks/cps.ts
@@ -0,0 +1,29 @@
+/**
+ * @file cps 相关 hooks
+ * @author jiayizhen <jiayizhen@qiniu.com>
+ */
+
+import { useEffect } from 'react'
+
+import { useQueryValue } from 'hooks/url'
+import { useApi } from 'hooks/api'
+
+import { setCpsKeyCookie } from 'apis/cps'
+
+export function useSetCpsKeyCookie() {
+  // 如果 url 有 cps_key query 参数
+  // 则调用后端接口种下对应的 cookie
+  // https://jira.qiniu.io/browse/BO-15948
+  // TODO: 后续将对应逻辑迁移到 externals 中
+  // https://jira.qiniu.io/browse/BO-16041
+  const [cpsKey] = useQueryValue('cps_key', '')
+  const { call: callSetCpsKeyCookie } = useApi(setCpsKeyCookie)
+
+  useEffect(() => {
+    if (!cpsKey) return
+
+    callSetCpsKeyCookie({
+      cps_key: cpsKey
+    })
+  }, [cpsKey, callSetCpsKeyCookie])
+}
