ea1c324ec9137781a92e7cc7afb9724cb96d7684
war408705279 <408705279@qq.com>
2021-03-17 12:09:37 +0800

    将原本 useReportCpsVisit 逻辑移动到 components/CpsVisitReporter 中

--- front/2020/components/CpsVisitReporter/index.tsx
+++ front/2020/components/CpsVisitReporter/index.tsx
@@ -1,11 +1,29 @@
 /**
  * @file 上报 cps 访问信息组件
+ * @author jiayizhen <jiayizhen@qiniu.com>
  */
 
-import { useReportCpsVisit } from 'hooks/cps'
+import React, { useEffect } from 'react'
+
+import { useQueryValue } from 'hooks/url'
+import { useApi } from 'hooks/api'
+
+import { reportCpsVisit } from 'apis/cps'
 
 export default function CpsVisitReporter() {
-  useReportCpsVisit()
+  // 如果 url 有 cps_key query 参数
+  // 则调用后端接口种下对应的 cookie
+  // https://jira.qiniu.io/browse/BO-15948
+  const [cpsKey] = useQueryValue('cps_key', '')
+  const { call: callReportCpsVisit } = useApi(reportCpsVisit)
 
-  return null
+  useEffect(() => {
+    if (!cpsKey) return
+
+    callReportCpsVisit({
+      cps_key: cpsKey
+    })
+  }, [cpsKey, callReportCpsVisit])
+
+  return <div></div>
 }
