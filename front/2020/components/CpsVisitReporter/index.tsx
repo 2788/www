a55b77758ec42422995fe515c5d1b49beeae52ab
war408705279 <408705279@qq.com>
2021-03-17 14:54:34 +0800

    CpsVisitReporter 中使用 getUrlQueryValueByKey 获取 cps_key query 值

--- front/2020/components/CpsVisitReporter/index.tsx
+++ front/2020/components/CpsVisitReporter/index.tsx
@@ -5,7 +5,7 @@
 
 import React, { useEffect } from 'react'
 
-import { useQueryValue } from 'hooks/url'
+import { getUrlQueryValueByKey } from 'hooks/url'
 import { useApi } from 'hooks/api'
 
 import { reportCpsVisit } from 'apis/cps'
@@ -14,16 +14,20 @@ export default function CpsVisitReporter() {
   // 如果 url 有 cps_key query 参数
   // 则调用后端接口种下对应的 cookie
   // https://jira.qiniu.io/browse/BO-15948
-  const [cpsKey] = useQueryValue('cps_key', '')
   const { call: callReportCpsVisit } = useApi(reportCpsVisit)
 
   useEffect(() => {
+    // 注意，这边不能使用 useQueryValue 来获取 url 中的 cps_key query 值
+    // 因为在 例如：externals 组件中路由不能反映当前页面的 url
+    // 用 useQueryValue 会出现取不到值的情况
+    const cpsKey = getUrlQueryValueByKey('cps_key')
+
     if (!cpsKey) return
 
     callReportCpsVisit({
       cps_key: cpsKey
     })
-  }, [cpsKey, callReportCpsVisit])
+  }, [callReportCpsVisit])
 
   return <div></div>
 }
