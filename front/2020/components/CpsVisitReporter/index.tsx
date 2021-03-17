b68949c22aea7cb8b8bead90649d74078633567b
war408705279 <408705279@qq.com>
2021-03-17 13:33:08 +0800

    remove debug CpsVisitReporter

--- front/2020/components/CpsVisitReporter/index.tsx
+++ front/2020/components/CpsVisitReporter/index.tsx
@@ -11,22 +11,14 @@ import { useApi } from 'hooks/api'
 import { reportCpsVisit } from 'apis/cps'
 
 export default function CpsVisitReporter() {
-  console.log('------')
-  console.log('CpsVisitReporter entry')
   // 如果 url 有 cps_key query 参数
   // 则调用后端接口种下对应的 cookie
   // https://jira.qiniu.io/browse/BO-15948
   const [cpsKey] = useQueryValue('cps_key', '')
   const { call: callReportCpsVisit } = useApi(reportCpsVisit)
 
-  console.log('------')
-  console.log('url cps_key', cpsKey)
-
   useEffect(() => {
     if (!cpsKey) return
-
-    console.log('------')
-    console.log('call report cps_key', cpsKey)
 
     callReportCpsVisit({
       cps_key: cpsKey
