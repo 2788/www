11be5477ced2a31dae3550a8d69ea67304b1f4ae
任攀攀 <34532573+renpanpan5@users.noreply.github.com>
2021-06-30 17:13:54 +0800

    [RMBWEB-1763]admin 增加产品动态页面 (#2234)
    * admin 增加产品动态页面
    
    * 代码 review 问题修改
    
    * 优化补充
    
    Co-authored-by: renpanpan5 <renpanpan5@github.com>

---
+++ front/admin/src/components/Www/Product/News/store.ts
@@ -0,0 +1,109 @@
+/**
+ * @file          Product News store
+ * @description   index 所需的 store，handle前缀的表示为需要ToasterStore.handle装饰的处理函数
+ * @author        renpanpan
+ */
+
+import { action, computed, observable } from 'mobx'
+import autobind from 'autobind-decorator'
+import Store from 'qn-fe-core/store'
+import { injectable } from 'qn-fe-core/di'
+
+import Loadings from 'admin-base/common/stores/loadings'
+import ModalStore from 'admin-base/common/stores/modal'
+import ToasterStore from 'admin-base/common/stores/toaster'
+
+import NewsApis, { INewsWithId, IListOptions, IListResponse } from 'apis/product/news'
+import PageApis, { IPage } from 'apis/product/page'
+import { EditorStatus } from 'constants/editor'
+
+import { ExtraProps } from './Editor'
+import { pageSize } from '.'
+
+@injectable()
+export default class NewsStore extends Store {
+
+  constructor(
+    private newsApis: NewsApis,
+    private pageApis: PageApis,
+    public toasterStore: ToasterStore
+  ) {
+    super()
+    ToasterStore.bind(this, toasterStore)
+  }
+
+  editorModal = new ModalStore<ExtraProps>()
+  @observable total = 0
+  @observable currentPage = 1
+  @observable.ref list: INewsWithId[] = []
+  @observable.ref pageList: IPage[] = []
+  loadings = Loadings.collectFrom(this)
+
+  @computed
+  get isLoading() {
+    return !this.loadings.isAllFinished()
+  }
+
+  @action.bound
+  updateCurrentPage(page: number) {
+    this.currentPage = page
+  }
+
+  @action.bound
+  updateList(total = 0, list: INewsWithId[]) {
+    this.total = total
+    this.list = list
+  }
+
+  @action.bound
+  updatePageList(list: IPage[]) {
+    this.pageList = list
+  }
+
+  @autobind
+  @Loadings.handle('fetchList')
+  fetchList({ page, products, types }: Omit<IListOptions, 'limit' | 'offset'> & { page: number }) {
+    const opt: IListOptions = { limit: pageSize, offset: (page - 1) * pageSize, products, types }
+    return this.newsApis.list(opt).then((res: IListResponse) => this.updateList(res.count, res.data))
+  }
+
+  // 获取产品页列表
+  @autobind
+  @Loadings.handle('fetchPageList')
+  fetchPageList() {
+    return this.pageApis.list().then(this.updatePageList)
+  }
+
+  @autobind
+  @ToasterStore.handle()
+  handleFetchList(opt: Omit<IListOptions, 'limit' | 'offset'> & { page: number }) {
+    return this.fetchList(opt)
+  }
+
+  @autobind
+  @ToasterStore.handle()
+  handleAdd() {
+    return this.editorModal.open({ status: EditorStatus.Creating }).then(() => this.fetchList({ page: 1 }))
+  }
+
+  @autobind
+  @ToasterStore.handle()
+  handleEdit(id: string) {
+    const news = this.list.find(item => item._id === id)
+    return this.editorModal.open({ news, id, status: EditorStatus.Editing }).then(() => this.fetchList({ page: 1 }))
+  }
+
+  @autobind
+  @ToasterStore.handle('删除产品动态成功')
+  handleDelete(id: string) {
+    return this.newsApis.delete(id).then(() => this.fetchList({ page: 1 }))
+  }
+
+  @ToasterStore.handle()
+  async init() {
+    this.addDisposer(
+      () => this.editorModal.dispose()
+    )
+    await Promise.all([this.fetchPageList(), this.fetchList({ page: 1 })])
+  }
+}
--- front/admin/src/components/Www/Product/index.tsx
+++ front/admin/src/components/Www/Product/index.tsx
@@ -4,10 +4,12 @@ import TabBar from 'admin-base/common/components/TabBar'
 
 import Page from './Page'
 import Notice from './Notice'
+import News from './News'
 
 const tabs = [
   { tab: '页面管理', path: '/page', content: <Page /> },
-  { tab: '产品公告', path: '/notice', content: <Notice /> }
+  { tab: '产品公告', path: '/notice', content: <Notice /> },
+  { tab: '产品动态', path: '/news', content: <News /> }
 ]
 
 export default observer(function Product() {
