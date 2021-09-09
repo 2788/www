792488d9606c95a0a5f50c7cc6db69bdfcecba6c
Haduo丶 <63768885+haduo0825@users.noreply.github.com>
2021-09-09 16:58:21 +0800

    revert-2231 (#2363)

--- front/2020/components/Price/kodo/Price/index.tsx
+++ front/2020/components/Price/kodo/Price/index.tsx
@@ -17,7 +17,7 @@ import Region, { nameMap } from '../region'
 
 export default function Price() {
   const isMobile = useMobile()
-  const packageLink = 'https://qmall.qiniu.com/template/MTEy?spec_combo=MzA0Mw'
+  const packageLink = 'https://qmall.qiniu.com/template/NA?spec_combo=Nzk&ref=kodo-price'
 
   return (
     <PricePane>
