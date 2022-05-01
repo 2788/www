0ef216cc6caef8f740030f6d49347f07968e9363
war408705279 <408705279@qq.com>
2022-05-01 13:42:53 +0800

    feat: 添加 feedback-entry-v3 externals 组件

---
+++ front/2020/externals/feedback-entry-v3.tsx
@@ -0,0 +1,52 @@
+/**
+ * @file 用户反馈入口 V3
+ * @description 用户反馈入口（含滚动到顶部的按钮）对应的 HTML 片段，用于外部集成
+ */
+
+import React, { useEffect } from 'react'
+
+import Layout from 'components/Layout/External'
+import { EntryV3 as FeedbackEntry, useModal } from 'components/Feedback'
+import ScrollToTop from 'components/ScrollToTop'
+
+import { register } from './helper'
+
+// 调起反馈浮层的函数
+let showModalFn: (() => void) | undefined
+
+/**
+ * 把用于调起反馈浮层的函数扔出来
+ * 方便 external 的嵌入方通过代码逻辑调起反馈浮层
+ */
+function FeedbackInvoker() {
+  const { startConsulting } = useModal()
+
+  useEffect(() => {
+    showModalFn = startConsulting
+
+    return () => {
+      showModalFn = undefined
+    }
+  }, [startConsulting])
+
+  return null
+}
+
+register('feedback-entry-v3', () => (
+  <Layout>
+    <FeedbackInvoker />
+    <FeedbackEntry />
+    <ScrollToTop />
+  </Layout>
+), {
+  showModal() {
+    if (!showModalFn) {
+      // eslint-disable-next-line no-console
+      console.warn('showModal() not ready yet')
+
+      return
+    }
+
+    showModalFn()
+  }
+})
