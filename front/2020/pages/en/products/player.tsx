3d2d4d09c79ab087c7758b3ab9752cd3bdafb534
Zhifeng Li <lzfee0227@gmail.com>
2022-05-30 16:43:47 +0800

    redirect legacy urls (#2576)

---
+++ front/2020/pages/en/products/player.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Redirect from 'components/Redirect'
+
+// 老官网页面重定向
+export default function LegacyRedirect() {
+  return (
+    <Redirect target="/products/qnplayer" />
+  )
+}
---
+++ front/2020/pages/events/qvm1rmb.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Redirect from 'components/Redirect'
+
+// 老官网页面重定向
+export default function LegacyRedirect() {
+  return (
+    <Redirect target="https://marketing.qiniu.com/activity/qvm0rmbv2" />
+  )
+}
