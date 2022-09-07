a0322cccda645d373a535405ca6c8285ab04cd20
cztbb <86031538+cztbb@users.noreply.github.com>
2022-09-07 18:23:10 +0800

    RMBWEB-3019 【官网】公安网警logo (#2633)
    * logo 上面添加文字，logo以及文字点击跳转
    
    * Link 代替 a
    
    * 删除多余空格
    
    * z-index 改用变量

--- front/2020/components/netPolice/index.tsx
+++ front/2020/components/netPolice/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useMobile } from 'hooks/ua'
+import Link from 'components/Link'
+
 import netPolice from './net_police.jpg'
 
 import styles from './style.less'
@@ -10,8 +12,10 @@ export default function NetPolice() {
     return null
   }
   return (
-    <div className={styles.netPolice}>
+    <Link className={styles.netPolice} href="https://www.qiniu.com/pgc/detail/7c0cec0a85595eb09749d9ef">
+      <div>国家网络</div>
+      <div>安全宣传周</div>
       <img src={netPolice} alt="公安网警" />
-    </div>
+    </Link>
   )
 }
