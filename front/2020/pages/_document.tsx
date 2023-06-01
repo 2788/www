7dc46fc761ea5e95a89e64368bec2ab4429c2020
chennan <chennan@qiniu.com>
2023-06-01 14:09:38 +0800

    miku-delivery v1.1.0 (#2767)
    * miku-delivery v1.1.0
    
    * fix: miku delivery script src

--- front/2020/pages/_document.tsx
+++ front/2020/pages/_document.tsx
@@ -2,7 +2,6 @@
 
 import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import { assetHost } from 'constants/env'
 
 const sensorsScriptContent = `
 (function(para) {
@@ -79,7 +78,7 @@ const mikuDeliveryInitScriptContent = `
   const debug = window.location.search.includes('debug')
   const appInfo = { appID: 'ao89rvrjpyi4gn57', appSalt: 'reimp7tnc2y9p11hckwz10lt3tigw8e7' }
   mikuDeliveryPerf.init(appInfo)
-  mikuDelivery.initProxy('/miku-delivery-sw-1.0.0.js', {
+  mikuDelivery.initProxy('/miku-delivery-sw-1.1.0.js', {
     app: appInfo,
     domains: ['www-static.qbox.me'],
     debug
@@ -98,8 +97,8 @@ class MyDocument extends Document {
           <script dangerouslySetInnerHTML={{ __html: gaScriptContent }} />
           <script dangerouslySetInnerHTML={{ __html: baiduhmScriptContent }} />
           <script dangerouslySetInnerHTML={{ __html: baiduzhanzhangScriptContent }} />
-          <script src={`${assetHost}/miku-delivery-perf-1.0.0.js`} />
-          <script src={`${assetHost}/miku-delivery-1.0.0.js`} />
+          <script src="/miku-delivery-perf-1.1.0.js" />
+          <script src="/miku-delivery-1.1.0.js" />
           <script dangerouslySetInnerHTML={{ __html: mikuDeliveryInitScriptContent }} />
         </Head>
         <body>
