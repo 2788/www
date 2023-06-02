e1fedccb82b183da59696ee47b3a5a8033a2198d
zzz <72262930+pingchangxin3z@users.noreply.github.com>
2023-06-02 16:07:36 +0800

    [RMBWEB-3324]短视频解决方案页面下掉几个链接 (#2768)
    * 下线南京办公室地址
    
    * fix
    
    * 短视频解决方案页面下掉几个链接
    
    ---------
    
    Co-authored-by: zhangzuzhou <“1760189000@qq.com”>

--- front/2020/components/pages/plsv-solution/Solution/Pc.tsx
+++ front/2020/components/pages/plsv-solution/Solution/Pc.tsx
@@ -35,16 +35,6 @@ export default function Pc() {
               <div className={styles.desc}>
                 灵活上传，快速转码，提供服务端 H.265 和 H.264 互转码服务。
               </div>
-              <div className={styles.desc}>
-                相关文档：
-                <a
-                  className={styles.link}
-                  href="https://developer.qiniu.com/dora/api/1249/real-time-audio-and-video-transcoding-avvod"
-                  title="实时转码"
-                >
-                  实时转码
-                </a>
-              </div>
             </div>
           </div>
         </div>
@@ -56,16 +46,6 @@ export default function Pc() {
               <div className={styles.desc}>
                 利用 AI 技术对视频内容进行智能分析。开发者可使用色情识别、暴恐识别、政治人物识别来智能审核视频，大大提高运营效率。
               </div>
-              <div className={styles.desc}>
-                相关文档：
-                <a
-                  className={styles.link}
-                  href="/products/dora"
-                  title="智能多媒体服务"
-                >
-                  智能多媒体服务
-                </a>
-              </div>
             </div>
           </div>
           <div className={styles.item}>
@@ -74,16 +54,6 @@ export default function Pc() {
               <div className={styles.title}>大数据运营</div>
               <div className={styles.desc}>
                 大数据实时监控各个性能指标，同步分析用户行为数据，助力短视频平台完善业务。
-              </div>
-              <div className={styles.desc}>
-                相关文档：
-                <a
-                  className={styles.link}
-                  href="/products/pandora"
-                  title="大数据"
-                >
-                  大数据
-                </a>
               </div>
             </div>
           </div>
